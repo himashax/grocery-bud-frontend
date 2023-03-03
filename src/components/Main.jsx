b089994a47d23cadab2d371a77cf167bdc54ec83
himasha <himasha.mandakini@gmail.com>
2023-03-03 11:52:25 +0530

    POST implementation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import Grocery_List from './Grocery-List'; 
 import instance from './axios';
+import axios from 'axios';
 
 function Main() {
 
+    const [item, setItem] = useState("");
     const [groceryList, setGroceryList] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newItem  = {
+            id: new Date().getTime().toString(),
+            name: item
+        }
+
+        instance.post('/add', newItem)
+        .then(response => {
+            console.log("Status: ", response.status);
+            console.log("Data: ", response.data);
+        }).catch(error => {
+            console.error('Something went wrong!', error);
+        });
+
+        console.log(newItem)
+    }
+
+    
 
     useEffect(() => {
         async function fetchData() {
@@ -12,17 +35,21 @@ function Main() {
           setGroceryList(request.data);
         }
         fetchData();
-      }, [])
+    }, [])
+
+
 
     return ( 
         <section className='section-center'>
-        <form className='grocery-form' >
+        <form className='grocery-form' onSubmit={handleSubmit} >
         <h3>grocery bud</h3>
         <div className="form-control">
             <input 
                 type="text" 
                 className='grocery' 
-                placeholder='e.g. eggs' />   
+                placeholder='e.g. eggs'
+                value={ item }
+                onChange={(e) => setItem(e.target.value)} />   
             <button type='submit' className="submit-btn">
             </button>
         </div>
